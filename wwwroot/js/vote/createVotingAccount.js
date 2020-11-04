6f549f96b956f98a5d8c21f22f970650d59aeaf7
Raskolnikov <hristov.pavel88@gmail.com>
2020-11-04 19:41:46 +0200

    finalize create voting account page

--- wwwroot/js/vote/createVotingAccount.js
+++ wwwroot/js/vote/createVotingAccount.js
@@ -1,12 +1,21 @@
 ﻿var createBtn = $('#create');
 var idnumber = $("#idnumber");
 var cardnumber = $("#cardnumber");
+var spinner = $("#spinner");
+spinner.hide();
+var form = $("#form");
+var error = $("#error");
+error.hide();
+var created = $("#created");
+created.hide();
 
 createBtn.on('click', function (ev) {
     var jsonObj = {
         IdNumber: idnumber.val(),
         CardNumber: cardnumber.val()
     }
+    form.hide();
+    spinner.show();
     $.ajax({
         method: "POST",
         url: "/Vote/CreateAccount",
@@ -15,8 +24,15 @@ createBtn.on('click', function (ev) {
         contentType: "application/json; charset=utf-8",
         success: function (data) {
             console.log(data);
+            spinner.hide();
+            created.show();
+            var accountId = data.accountId;
+            var voteTokenBalance = data.voteTokenBalance;
+            $("#account-details").html(`Your accound id is ${accountId} and you have ${voteTokenBalance} Vote token.`);
         },
         error: function (err) {
+            spinner.hide();
+            error.show();
             console.log(err);
         }
     });
